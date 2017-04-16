22a3befb84ff72e6a2257ec958191d976f12288a
Lars Vollaers <info@larsvollaers.nl>
2017-04-16 02:52:32 +0200

    v2.0
    Added eshop games. Date: 16-04-2017

--- config/database.php
+++
@@ -1,5 +0,0 @@
-<?php
-$sitename = 'Nintendo eShop';
-$footer = "Copyright © ".$sitename." ".date("Y")." | Made by Lars Vollaers";
-?>
-
--- config/js/home.js
+++ config/js/home.js
@@ -4,13 +4,18 @@ $(document).ready(function(){
 	$( ".menu_close" ).click(function() {
 		$('.color_orange').animate({"margin-left": '-=25%'});
 		$( ".menu_toggler" ).addClass( "hide" );
-		$('.menu_special').removeClass('hide');
+		$('.menu_special').removeClass('hide');	
+		$( ".games_small" ).addClass( "games_big" );
+		$( ".games_small_soon" ).addClass( "games_big_soon" );
 	});
 	
 	$( ".menu_open" ).click(function() {
 		$('.color_orange').animate({"margin-left": '+=25%'});
 		$('.menu_toggler').removeClass('hide');
 		$( ".menu_special" ).addClass( "hide" );
+		$( ".games_small" ).removeClass( "games_big" );
+		$( ".games_small_soon" ).removeClass( "games_big_soon" );
+		
 	});
 
 });
