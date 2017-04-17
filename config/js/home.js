261a794a1c182cf09c23d143f0a229f03838df3b
Lars Vollaers <info@larsvollaers.nl>
2017-04-17 22:20:05 +0200

    v3.0
    added: Search engine.
    Fixed: using the navigation.

--- config/js/home.js
+++ config/js/home.js
@@ -1,12 +1,23 @@
 	
 $(document).ready(function(){	
 
+var clicks = 0;
+
+
 	$( ".menu_close" ).click(function() {
-		$('.color_orange').animate({"margin-left": '-=25%'});
-		$( ".menu_toggler" ).addClass( "hide" );
-		$('.menu_special').removeClass('hide');	
-		$( ".games_small" ).addClass( "games_big" );
-		$( ".games_small_soon" ).addClass( "games_big_soon" );
+		if(clicks == 1){
+			$('.color_orange').animate({"margin-left": '-=25%'});
+			$( ".menu_toggler" ).addClass( "hide" );
+			$('.menu_special').removeClass('hide');	
+			$( ".games_small" ).addClass( "games_big" );
+			$( ".games_small_soon" ).addClass( "games_big_soon" );
+			$( ".search_small" ).addClass( "search_big" );
+			clicks--;
+			clicks--;
+			//console.log("Clicks : ",clicks);
+		}
+		clicks++;
+		//console.log("Click : ",clicks);
 	});
 	
 	$( ".menu_open" ).click(function() {
@@ -15,7 +26,30 @@ $(document).ready(function(){
 		$( ".menu_special" ).addClass( "hide" );
 		$( ".games_small" ).removeClass( "games_big" );
 		$( ".games_small_soon" ).removeClass( "games_big_soon" );
+		$( ".search_small" ).removeClass( "search_big" );
 		
 	});
+	
+	
+	
+
 
 });
+
+function SearchGame() {
+    var input, filter, ul, li, a, i;
+    input = document.getElementById("SearchaGame");
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("GameList");
+    li = ul.getElementsByTagName("li");
+    for (i = 0; i < li.length; i++) {
+        a = li[i].getElementsByTagName("a")[0];
+        if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = "";
+			$( "#GameList" ).addClass( "GameList_hidden" );
+        } else {
+            li[i].style.display = "none";
+			$( "#GameList" ).removeClass( "GameList_hidden" );
+        }
+    }
+}
